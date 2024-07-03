c11ca8bcb8ad9fa2d9ebbadb55043750a5b9578f
Wilsiman Evangelista <wilsiman.evangelista.ifes@gmail.com>
2024-07-03 19:00:38 -0300

    Acertei os códigos

--- obt.py
+++ obt.py
@@ -17,14 +17,14 @@ def upload_file():
     
     file = request.files['file']
     if file.filename == '':
-        return jsonify({"error": "No selected file"}), 400
+        return jsonify({"error": "No selected file"}), 401
     
     if file and allowed_file(file.filename):
         filename = file.filename
         file.save(os.path.join(UPLOAD_FOLDER, filename))
         return jsonify({"message": "File uploaded successfully"}), 200
     
-    return jsonify({"error": "Invalid file format"}), 400
+    return jsonify({"error": "Invalid file format"}), 402
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
