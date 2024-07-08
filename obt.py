5a2f04f5619c6ea7e599cc7faec7858dc9c9eb46
Wilsiman <wilsiman.evangelista.ifes@gmail.com>
2024-07-08 17:55:48 -0300

    AAAA

--- obt.py
+++ obt.py
@@ -15,21 +15,15 @@ def upload_file():
         else:
             new_list_images = []
 
-
             combinedMultiDict_To_Lists = list(a.lists())
-            print(combinedMultiDict_To_Lists)
-
             list_Images = combinedMultiDict_To_Lists[0][0]
-            print(list_Images)
-            
+
             for i in json.loads(list_Images):
                 new_list_images.append(i.replace("\"","\'"))
-            print(new_list_images)
+            
+            main(file_names=new_list_images)
 
-            images_Download = main(file_names=new_list_images)
-            print(images_Download)
-
-            return list_Images,images_Download, 200
+            return "Ok", 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 401
