054e8677e58cc294257792ff64368ec53dd94850
Wilsiman <wilsiman.evangelista.ifes@gmail.com>
2024-07-08 17:45:03 -0300

    AAAAAAAAAAAAa

--- obt.py
+++ obt.py
@@ -11,12 +11,22 @@ def upload_file():
             print("AAAAAAAAAAA")
             return 0
         else:
+            new_list_images = []
+
+
             combinedMultiDict_To_Lists = list(a.lists())
             print(combinedMultiDict_To_Lists)
+
             list_Images = combinedMultiDict_To_Lists[0][0]
             print(list_Images)
-            images_Download = main(file_names=list_Images)
+            
+            for i in list_Images:
+                new_list_images.append(i.replace("\"","\'"))
+            print(new_list_images)
+
+            images_Download = main(file_names=new_list_images)
             print(images_Download)
+            
             return list_Images,images_Download, 200
 
     except Exception as e:
@@ -24,3 +34,4 @@ def upload_file():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
+
