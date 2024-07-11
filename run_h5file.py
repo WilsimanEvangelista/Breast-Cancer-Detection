30b55852e75bca1fb1368c14a12e54fe0d5b4a84
Wilsiman Evangelista <wilsiman.evangelista.ifes@gmail.com>
2024-07-11 16:29:23 -0300

    aaaaaaaaaaaaa

--- run_h5file.py
+++ run_h5file.py
@@ -23,6 +23,3 @@ def run_h5(image_path: str) -> (float, str):
     else:
         print(pred, "Tumor Benigno")
         return pred, "Tumor Benigno"
-    
-if __name__ == "__main__":
-    run_h5('tem.png')
