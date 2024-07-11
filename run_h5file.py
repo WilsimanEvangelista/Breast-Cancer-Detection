0adebfe30c801e8d0622192ac81ac9a4f6a81b5b
Wilsiman Evangelista <wilsiman.evangelista.ifes@gmail.com>
2024-07-11 09:35:41 -0300

    Teste - Rodar modelo .h5

---
+++ run_h5file.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+def main():
+    model_path = "model\\model.h5"
+    loaded_model = tf.keras.models.load_model(model_path)
+
+    image = cv2.imread("img.png")
+
+    image_fromarray = Image.fromarray(image, 'RGB')
+    resize_image = image_fromarray.resize((224, 224))
+    expand_input = np.expand_dims(resize_image,axis=0)
+    input_data = np.array(expand_input)
+    input_data = input_data/255
+
+    pred = loaded_model.predict(input_data)
+    if pred >= 0.5:
+        print("Yes")
+    else:
+        print("No")
+        
+if __name__ == "__main__":
+    main()
